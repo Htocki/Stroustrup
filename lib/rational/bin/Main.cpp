9c41da0d806b0abecea81431490091cc49b4322b
Htocki <pasztownia@gmail.com>
2020-02-16 22:47:13 +0300

    Correction.

--- lib/rational/bin/Main.cpp
+++ lib/rational/bin/Main.cpp
@@ -1,12 +1,16 @@
 #include "Rational.h"
 
 int main() {
-	Rational r;
-	Rational lhs{ 3, 2 };
-	Rational rhs{ 2, 3 };
+	Rational default;
+	Rational a{ 3, 2 };
+	Rational b{ 2, 3 };
 
-	std::cout << "lhs: " << lhs << std::endl
-		<< "rhs: " << rhs << std::endl;
+	std::cout
+		<< "rational a: " << a << std::endl
+		<< "rational b: " << b << std::endl
+		<< "double a: " << a.ToDouble() << std::endl
+		<< "double b: " << b.ToDouble() << std::endl
+		<< std::endl;
 
 	return 0;
 }
