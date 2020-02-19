43c285a573ac186f4b5e7ac2ff35a79493293ffe
pasztownia@gmail.com <pasztownia@gmail.com>
2020-02-19 20:04:24 +0300

    Fixed: "https://github.com/Htocki/Stroustrup/pull/5#discussion_r381117134"

--- lib/rational/bin/Main.cpp
+++ lib/rational/bin/Main.cpp
@@ -1,15 +1,15 @@
 #include "Rational.h"
 
 int main() {
-	Rational default;
-	Rational a{ 3, 2 };
-	Rational b{ 2, 3 };
+	Rational a;
+	Rational b{ 3, 2 };
+	Rational c{ 2, 3 };
 
 	std::cout
-		<< "rational a: " << a << std::endl
 		<< "rational b: " << b << std::endl
-		<< "double a: " << a.ToDouble() << std::endl
+		<< "rational c: " << c << std::endl
 		<< "double b: " << b.ToDouble() << std::endl
+		<< "double c: " << c.ToDouble() << std::endl
 		<< std::endl;
 
 	return 0;
