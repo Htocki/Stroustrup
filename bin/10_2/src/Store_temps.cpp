c9eabba4a41dac942a910342592ecf582175ebe1
Htocki <pasztownia@gmail.com>
2020-03-07 17:11:46 +0300

    Correction.

--- bin/10_2/src/Store_temps.cpp
+++ bin/10_2/src/Store_temps.cpp
@@ -4,56 +4,47 @@
 #include <iostream>
 #include <vector>
 
-constexpr std::uint64_t number_of_readings = { 50 };
+constexpr std::uint64_t number_of_readings = { 5 };
 
 struct Reading {
     int hour; // [0, 23].
     double temperature; // Fahrenheit.
 };
 
-Reading AddReading() {
-    try {
-        Reading r;
-        std::cout << "Input hour [0, 23]: ";
-        if (!(std::cin >> r.hour) || r.hour < 0 || r.hour > 23) {
-            throw std::exception{ 
-                "Input is incorrect.\n\n"
-                "Try again:"
-            };
-        }
+Reading InstanceReading() {
+    std::cin.exceptions(std::ios::failbit);
+    Reading r;
 
-        std::cout << "Input temperature (Fahrenheit): ";
-        if (!(std::cin >> r.temperature)) {
-            throw std::exception{
+    std::cout << "Input hour [0, 23]: ";
+    if (std::cin >> r.hour) {
+        if (r.hour < 0 || r.hour > 23) {
+            std::cerr <<
                 "Input is incorrect.\n\n"
-                "Try again:"
-            };
-        }
-
-        return r;
-    }
-    catch (std::exception e) {
-        if (std::cin.eof()) {
-            throw std::exception{ 
-                "The program stopped working because "
-                "you entered the end of file symbol." 
-            };
-        }
-        else {
-            std::cerr << e.what() << std::endl;
-            return AddReading();
+                "Try again:" << std::endl;
+            return InstanceReading();
         }
     }
 
+    std::cout << "Input temperature (Fahrenheit): ";
+    if (std::cin >> r.temperature) {
+        if (r.temperature < -1000 || r.temperature > 1000) {
+            std::cerr <<
+                "Input is incorrect.\n\n"
+                "Try again:" << std::endl;
+            return InstanceReading();
+        }
+    }
+
+    return r;
 }
 
 int main() {
-    std::vector<Reading> temps;
     try {
+        std::vector<Reading> temps;
         std::ofstream ofs("Raw_temps.txt");
         if (ofs.is_open()) {
             while (temps.size() != number_of_readings) {
-                temps.push_back(AddReading());
+                temps.push_back(InstanceReading());
             }
 
             for (auto r : temps) {
@@ -63,6 +54,9 @@ int main() {
         else {
             throw std::exception{ "Error opening file." };
         }
+    }
+    catch (std::ios_base::failure e) {
+        std::cerr << "Critical input error." << std::endl;
     }
     catch (std::exception e) {
         std::cerr << e.what() << std::endl;
