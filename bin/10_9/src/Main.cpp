eed79ab6d190a6a0b0f497be236f9f7aad4d315e
Htocki <pasztownia@gmail.com>
2020-03-16 17:52:41 +0300

    Exercise 9 of chapter 10 completed.

---
+++ bin/10_9/src/Main.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    std::vector<std::string> file_names;
+
+    for (std::size_t i{ 1 }; i < argc; ++i) {
+        file_names.push_back(argv[i]);
+    }
+
+    std::ifstream 
+        file_one{ file_names[0] }, 
+        file_two{ file_names[1] };
+
+    std::ofstream new_file ("NewFile.txt", std::ios_base::ate);
+
+    if (file_one.is_open() && 
+        file_two.is_open() &&
+        new_file.is_open()) 
+    {
+        std::vector<std::string> words;
+        std::string s;
+
+        while (file_one >> s) {
+            words.push_back(s);
+        }
+
+        while (file_two >> s) {
+            words.push_back(s);
+        }
+
+        std::sort(words.begin(), words.end());
+
+        for (const auto word : words) {
+            new_file << word << " ";
+        }
+    }
+    else {
+        std::cerr << "An error occurred while opening files." << std::endl;
+    }
+
+    // Pause.
+    // char c; std::cin.get(c);
+}
