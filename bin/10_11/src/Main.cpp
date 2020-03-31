40c241e1337d9d0e7a341ca469625411d6da49fb
Htocki <pasztownia@gmail.com>
2020-03-31 16:19:48 +0300

    The comments are implemented.

--- bin/10_11/src/Main.cpp
+++ bin/10_11/src/Main.cpp
@@ -1,34 +1,27 @@
 #include <cstdint>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <numeric>
 #include <sstream>
 #include <string>
 #include <vector>
 
-std::int64_t Sum(std::vector<std::int64_t>& values) {
-    std::int64_t sum = { 0 };
-    for (const auto value : values) {
-        sum += value;
-    }
-    return sum;
-}
-
 int main() {
-    std::ifstream file("files/File.txt");
-    if (file.is_open()) {
-        std::vector<std::int64_t> values;
-        for (std::string string; file >> string; ) {
-            std::stringstream ss;
-            ss << string;
-            std::int64_t value;
-            if (ss >> value) {
-                values.push_back(value);
-            }
-        }
-        std::cout << "Sum: " << Sum(values) << std::endl;
+  std::ifstream file("files/File.txt");
+  if (file.is_open()) {
+    std::vector<std::int64_t> values;
+    for (std::string string; file >> string;) {
+      std::stringstream ss;
+      ss << string;
+      std::int64_t value { 0 };
+      if (ss >> value) { values.push_back(value); }
     }
-    else {
-        std::cerr << "Error opening file." << std::endl;
+    std::cout
+      << "Summation Result: "
+      << std::accumulate(values.begin(), values.end(), 0)
+      << std::endl;
+    } else {
+      std::cerr << "Error opening file." << std::endl;
     }
-    return 0;
+  return 0;
 }
