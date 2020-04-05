ab0864d4a6ca05afc526520f4cde90546ead9311
Htocki <pasztownia@gmail.com>
2020-04-05 20:42:24 +0300

    Edit style.

--- bin/11_1/src/Main.cpp
+++ bin/11_1/src/Main.cpp
@@ -4,22 +4,22 @@
 #include <cctype>
 
 int main() {
-    try {
-        std::ifstream inputFile { "files/Input.txt" };
-        std::ofstream outputFile { "files/Out.txt" };
-        if (inputFile.is_open() && outputFile.is_open()) {
-            for (char c { ' ' }; inputFile.get(c);) {
-                outputFile.put(std::tolower(c));
-            }
-        } else {
-            throw std::invalid_argument {
-                "The specified file was not found."
-            };
-        }
+  try {
+    std::ifstream inputFile { "files/Input.txt" };
+    std::ofstream outputFile { "files/Out.txt" };
+    if (inputFile.is_open() && outputFile.is_open()) {
+      for (char c { ' ' }; inputFile.get(c);) {
+        outputFile.put(std::tolower(c));
+      }
+    } else {
+      throw std::invalid_argument {
+        "The specified file was not found."
+      };
     }
-    catch (std::invalid_argument& e) {
-        std::cout << e.what() << std::endl;
-    }
+  }
+  catch (std::invalid_argument& e) {
+    std::cout << e.what() << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
