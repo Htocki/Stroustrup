a4c2b3556a6561c91039c4c27d74d7d72837923c
Htocki <pasztownia@gmail.com>
2020-04-05 20:15:33 +0300

    Exercise 1 of chapter 11 completed.

---
+++ bin/11_1/src/Main.cpp
@@ -0,0 +1,25 @@
+#include <exception>
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+int main() {
+    try {
+        std::ifstream inputFile { "files/Input.txt" };
+        std::ofstream outputFile { "files/Out.txt" };
+        if (inputFile.is_open() && outputFile.is_open()) {
+            for (char c { ' ' }; inputFile.get(c);) {
+                outputFile.put(std::tolower(c));
+            }
+        } else {
+            throw std::invalid_argument {
+                "The specified file was not found."
+            };
+        }
+    }
+    catch (std::invalid_argument& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
