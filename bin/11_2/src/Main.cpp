6ac578cc219e233d6c041ff5043484c394df778a
Htocki <pasztownia@gmail.com>
2020-04-08 16:59:54 +0300

    Exercise 3 of chapter 11 completed.

--- bin/11_2/src/Main.cpp
+++ bin/11_2/src/Main.cpp
@@ -4,6 +4,29 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <string_view>
+#include <vector>
+
+bool IsSeparator(const char symbol) {
+  const std::string_view separators { " .,!?;:'\"[](){}<>/|\\" };
+  return separators.find(symbol) != std::string_view::npos;
+}
+
+// A word is a set of characters separated from the
+// rest by a set of separator characters.
+bool IsWordFound(const std::string_view word, const std::string_view line) {
+  auto begin = line.find(word);
+  if (begin == std::string_view::npos) {
+    return false;
+  } else {
+    auto end = begin + word.size() - 1;
+    // The case when a word occupies the entire line.
+    if (begin == 0 && end == line.size()) { return true; }
+
+    return IsSeparator(line.at(begin - 1))
+      && IsSeparator(line.at(end + 1));
+  }
+}
 
 int main() {
   try {
@@ -17,16 +40,16 @@ int main() {
       std::cout << "Input word: ";
       std::getline(std::cin, word);
 
-      std::string string;
-      bool strings_found { false };
-      for (std::int64_t index { 1 }; std::getline(file, string); ++index) {
-        if (string.find(word) != std::string::npos) {
+      std::string line;
+      bool lines_with_word_is_found { false };
+      for (std::int64_t index { 1 }; std::getline(file, line); ++index) {
+        if (IsWordFound(word, line)) {
           std::cout << " [" << index << "] \t"
-            << "\"" << string << "\"" << std::endl;
-          strings_found = true;
+            << "\"" << line << "\"" << std::endl;
+          lines_with_word_is_found = true;
         }
       }
-      if (!strings_found) {
+      if (!lines_with_word_is_found) {
         std::cout << "No strings containing the specified"
           " word were found in the file." << std::endl;
       }
