9e2c1a6f7123d6d129978202b3a48ad6ac1236a3
Htocki <pasztownia@gmail.com>
2020-04-13 13:58:37 +0300

    The comments are implemented.

--- bin/11_1/src/Main.cpp
+++ bin/11_1/src/Main.cpp
@@ -1,23 +1,31 @@
+#include <algorithm>
 #include <cctype>
 #include <exception>
 #include <iostream>
+#include <iterator>
 #include <fstream>
 
 int main() {
   try {
-    std::ifstream inputFile { "files/Input.txt" };
-    std::ofstream outputFile { "files/Out.txt" };
-    if (inputFile.is_open() && outputFile.is_open()) {
-      for (char c { ' ' }; inputFile.get(c);) {
-        outputFile.put(std::tolower(c));
-      }
-    } else {
+    std::ifstream in { "files/In.txt" };
+    std::ofstream out { "files/Out.txt" };
+
+    if (!in.is_open()) {
       throw std::invalid_argument {
-        "The specified file was not found."
+        "The file \"files/In.txt\" was not found."
       };
     }
+
+    in >> std::noskipws;
+    out << std::noskipws;
+
+    std::transform(
+      std::istream_iterator<char> (in),
+      std::istream_iterator<char> (),
+      std::ostream_iterator<char> (out),
+      [] (char c) { return std::tolower(c); } );
   }
-  catch (std::invalid_argument& e) {
+  catch (const std::invalid_argument& e) {
     std::cout << e.what() << std::endl;
   }
 
