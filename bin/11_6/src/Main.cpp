f90024c48879e5442ce125fdb8906b64d8c386c7
Htocki <pasztownia@gmail.com>
2020-04-13 15:39:15 +0300

    Saving sketches.

--- bin/11_6/src/Main.cpp
+++ bin/11_6/src/Main.cpp
@@ -5,10 +5,42 @@
 // кавычек ("). Например, строка "  - don't use the as-if rule."
 // должна принять вид "  don t use the as if rule ".
 
+#include <algorithm>
+#include <cctype>
 #include <exception>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <string>
+
+bool IsPunct(const unsigned char c) {
+  return c == '.' || c == ';' || c == ',' || c == '?' ||
+    c == '-' || c == '\'';
+}
 
 int main() {
+  try {
+    std::ifstream file { "files/File.txt" };
+    if (!file.is_open()) {
+      throw std::invalid_argument {
+        "The file \"files/File.txt\" was not found."
+      };
+    }
+
+    file >> std::noskipws;
+    std::string buffer;
+    std::transform(
+      std::istream_iterator<char> (file),
+      std::istream_iterator<char> (),
+      std::back_inserter(buffer),
+      [] (unsigned char c) -> unsigned char {
+        if (IsPunct(c)) { c = ' '; }
+        return c;
+      });
+
+    std::cout << std::noskipws << buffer << std::endl;
+  } catch (const std::invalid_argument& e) {
+    std::cerr << e.what() << std::endl;
+  }
   return 0;
 }
