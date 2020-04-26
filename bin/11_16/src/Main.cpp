faac9be0c1399a3d5d11ceb27c18a0ba9d170921
Htocki <pasztownia@gmail.com>
2020-04-27 00:36:47 +0300

    Correction.

--- bin/11_16/src/Main.cpp
+++ bin/11_16/src/Main.cpp
@@ -6,7 +6,9 @@
 #include <utility>
 #include <vector>
 
-void Print(const std::vector<std::pair<int, int>>& values) {
+using Pair = std::pair<int, int>;
+
+void Print(const std::vector<Pair>& values) {
   for (const auto value : values) {
     std::cout << value.first;
     if (value.second > 1) {
@@ -16,8 +18,8 @@ void Print(const std::vector<std::pair<int, int>>& values) {
   }
 }
 
-std::vector<std::pair<int, int>> Repack(const std::vector<int>& values) {
-  std::vector<std::pair<int, int>> resorted_values;
+std::vector<Pair> Repack(const std::vector<int>& values) {
+  std::vector<Pair> resorted_values;
   for (const auto value : values) {
     resorted_values.push_back({
       value,
