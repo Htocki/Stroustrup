ef6aad2089a7c597ea74ba84eeaafb5a7a61f7ac
Htocki <48097100+Htocki@users.noreply.github.com>
2020-05-16 17:00:32 +0300

    Exercise 9 of chapter 11 completed. (#22)
    Exercise 9 of chapter 11 completed.

---
+++ bin/11_9/src/Main.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <bitset>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <string_view>
+#include <vector>
+
+template<class T>
+char* AsBytes(T& i) {
+  void* addr = &i;
+  return static_cast<char*>(addr);
+}
+
+bool IsToBinary(std::string_view flag) {
+  return flag == "-tB" || flag == "--to-binary";
+}
+
+bool IsToTextual(std::string_view flag) {
+  return flag == "-tT" || flag == "--to-textual";
+}
+
+bool IsFlag(std::string_view flag) {
+  return IsToBinary(flag) || IsToTextual(flag);
+}
+
+int main(int argc, char* argv[]) {
+  try {
+    if (argc != 3) {
+      throw std::invalid_argument {
+        "Invalid number of parameters specified."
+      };
+    }
+
+    const std::string_view flag { argv[1] };
+    if (!IsFlag(flag)) {
+      throw std::invalid_argument {
+        "The specified character set is not a flag."
+      };
+    }
+
+    if (IsToBinary(flag)) {
+      std::ifstream textual { argv[2] };
+      if (!textual.is_open()) {
+        throw std::invalid_argument {
+          "The specified file was not found."
+        };
+      }
+      std::ofstream binary { "file.bin", std::ios::binary };
+      std::for_each(
+        std::istream_iterator<int> { textual },
+        std::istream_iterator<int> { },
+        [&binary] (auto value) {
+          binary.write(AsBytes(value), sizeof(int));
+        });
+    }
+
+    if (IsToTextual(flag)) {
+      std::ifstream binary { argv[2], std::ios::binary };
+      if (!binary.is_open()) {
+        throw std::invalid_argument {
+          "The specified file was not found."
+        };
+      }
+      std::vector<int> values;
+      for (int value { 0 }; binary.read(AsBytes(value), sizeof(int));) {
+        values.push_back(value);
+      }
+      std::ofstream textual { "file.txt" };
+      std::copy(
+        std::begin(values),
+        std::end(values),
+        std::ostream_iterator<int> { textual, " " });
+    }
+  }
+  catch (const std::invalid_argument& excaption) {
+    std::cerr << excaption.what() << std::endl;
+  }
+  catch (const std::exception& excaption) {
+    std::cerr << excaption.what() << std::endl;
+  }
+  catch (...) {
+    std::cout << "Unknown exception." << std::endl;
+  }
+  return 0;
+}
